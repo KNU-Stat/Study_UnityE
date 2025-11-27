0426bc5d3ec1ee3103a95260398575780857da66
Heywon Ahn <frozenorb998@gmail.com>
2025-11-27 13:47:14 +0900

    Day2 -1

--- Project_E/Assets/Scenes/Study.cs
+++ Project_E/Assets/Scenes/Study.cs
@@ -1,11 +1,32 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Study : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public string say = "";
+    private string said = "";
+    string newsay = ("Hello! World.");
+    public char text;
+    public int var01 = 1;
+    public float var02 = 0.4f;
+    public double var03 = 0.5;
+    public bool var04 = true;
+    private bool var05 = false;
+
     void Start()
+
     {
-        Debug.Log("Hello, World!");
+        int nevar = 10;
+        Debug.Log(nevar);
+        Debug.Log(nevar++);
+        Debug.Log(++nevar);
+
+        Debug.Log(Random.Range(1,100));
+
+        Debug.Log(said);
+        Debug.Log(say);
+        Debug.Log(newsay);
+
     }
 
     // Update is called once per frame
